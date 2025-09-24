590fa283282b234b4125d24d4442886c5cd096bf
IronPirate99 <ugowin777@gmail.com>
2025-09-24 15:15:55 +0200

    changed DB Connection String

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const Mongoose = require('mongoose'); // Install mongoose library so that we can use mongoose methods 
 
-Mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
+Mongoose.connect('mongodb://localhost:27017/MovieDB2', { useNewUrlParser: true, useUnifiedTopology: true });
 
 "{ useNewUrlParser: true, useUnifiedTopology: true }); --> These are options to avoid deprecation warnings in Mongoose."
 
 module.exports = Mongoose;
 // Exporting the Mongoose object to use it in other files
+// change connection string to your own database name movieDB2
